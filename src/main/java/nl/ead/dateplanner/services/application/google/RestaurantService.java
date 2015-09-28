7b0a2efa3f478039d26a50f4f1ec8bcc17323fde
Glowbal <thomasskooi@live.nl>
2015-09-28 21:03:05 +0200

    Initial set up of the RestaurantService.
    - Add sprockets dependency for usage of the google places api
    - Add basic handling of retrieving places for a location (city name)
    - Add sprockets.xml to the gitignore. Contains the Google API key.

--- src/main/java/nl/ead/dateplanner/services/application/google/RestaurantService.java
+++ src/main/java/nl/ead/dateplanner/services/application/google/RestaurantService.java
@@ -1,3 +1,26 @@
+package nl.ead.dateplanner.services.application.google;
+
+import net.sf.sprockets.google.Place;
+import net.sf.sprockets.google.Places;
+
+import java.io.IOException;
+import java.util.List;
+
 public class RestaurantService {
-	
+
+    public RestaurantService() { // TODO provide mock object for places API wrapper?
+
+    }
+
+	public static String getRestaurantsNearPlace(String place) {
+        String ret = "";
+        try {
+            List<Place> places = Places.textSearch(new Places.Params().query(place).keyword("restaurant")).getResult();
+
+            return places.get(0).getName();
+        } catch (IOException e) {
+            // handle exception
+        }
+        return ret;
+    }
 }
