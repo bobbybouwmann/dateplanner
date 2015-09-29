e3f587aeb78734bd603d562a10f74ece411f435a
Bobby Bouwmann <bobbybouwmann@gmail.com>
2015-09-29 12:40:48 +0200

    Initial setup for weather service

--- src/main/java/nl/ead/dateplanner/services/application/DateFinderService.java
+++ src/main/java/nl/ead/dateplanner/services/application/DateFinderService.java
@@ -1,3 +1,14 @@
+package nl.ead.dateplanner.services.application;
+
+import nl.ead.dateplanner.services.application.yahoo.WeatherService;
+
 public class DateFinderService {
+
+    public String findWeather()
+    {
+        WeatherService weatherService = new WeatherService();
+
+        return weatherService.WeatherService();
+    }
 	
 }
--- src/main/java/nl/ead/dateplanner/services/application/yahoo/WeatherService.java
+++ src/main/java/nl/ead/dateplanner/services/application/yahoo/WeatherService.java
@@ -1,3 +1,23 @@
+package nl.ead.dateplanner.services.application.yahoo;
+
+import com.github.fedy2.weather.YahooWeatherService;
+import com.github.fedy2.weather.data.Channel;
+import com.github.fedy2.weather.data.unit.DegreeUnit;
+
 public class WeatherService {
+
+    public String WeatherService() {
+        Channel channel = null;
+
+        try {
+            YahooWeatherService service = new YahooWeatherService();
+
+            channel = service.getForecast("2502265", DegreeUnit.CELSIUS);
+        } catch(Exception e) {
+            System.err.println("Exception: " + e.getMessage());
+        }
+
+        return channel.getDescription();
+    }
 	
 }
