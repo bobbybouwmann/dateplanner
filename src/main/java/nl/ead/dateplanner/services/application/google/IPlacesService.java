995793655ca59c51bb858bcc6de2898b76bce1ad
Glowbal <thomasskooi@live.nl>
2015-10-03 14:01:50 +0200

    Change IPlacesService interface to support new methods

--- src/main/java/nl/ead/dateplanner/services/application/google/IPlacesService.java
+++ src/main/java/nl/ead/dateplanner/services/application/google/IPlacesService.java
@@ -1,8 +1,7 @@
 package nl.ead.dateplanner.services.application.google;
 
-import net.sf.sprockets.google.Place;
-
 import java.util.List;
+import nl.ead.dateplanner.services.application.google.Place;
 
 /**
  * Created by Thomas Kooi on 1-10-2015.
@@ -10,9 +9,10 @@ import java.util.List;
 public interface IPlacesService {
 
     /**
-     * Get the restaurants near a given place
-     * @param place Query name of a city or location. For example: "Amsterdam, nl"
-     * @return Restaurants near by given city or location.
+     * Get the places near a given location of given type
+     * @param type The type of place to be searched for
+     * @param location Query name of a city or location. For example: "Amsterdam, nl"
+     * @return Places near by given city or location of given type.
      */
-    List<Place> getRestaurantsNearPlace(String place);
+    public List<Place> getPlacesNearLocation(String type, String location);
 }
