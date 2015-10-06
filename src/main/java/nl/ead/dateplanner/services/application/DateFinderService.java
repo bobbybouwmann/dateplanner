04a8c47d98ae5fd3687a16178d4cd8c707702112
Glowbal <thomasskooi@live.nl>
2015-10-06 13:14:08 +0200

    Add async calls to external api

--- src/main/java/nl/ead/dateplanner/services/application/DateFinderService.java
+++ src/main/java/nl/ead/dateplanner/services/application/DateFinderService.java
@@ -3,10 +3,16 @@ package nl.ead.dateplanner.services.application;
 import nl.ead.dateplanner.services.application.google.GeoLocation;
 import nl.ead.dateplanner.services.application.google.IGeoLocationService;
 import nl.ead.dateplanner.services.application.google.IPlacesService;
+import nl.ead.dateplanner.services.application.google.Place;
+import nl.ead.dateplanner.services.application.openweather.Forecast;
 import nl.ead.dateplanner.services.application.openweather.IWeatherService;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.concurrent.Future;
 
 public class DateFinderService implements IDateFinderService {
 
@@ -29,13 +35,29 @@ public class DateFinderService implements IDateFinderService {
         BigDecimal latitude = geoLocation.latitude;
 
         try {
-            dateOption.places = placesService.getPlacesNearLocation(latitude, longitude, type, radius);
-            dateOption.forecasts = weatherService.retrieveWeather(latitude, longitude, dayPart);
-        } catch (IOException e) {
+            Future<List<Place>> places = getPlacesNearLocation(type, radius, longitude, latitude);
+            Future<List<Forecast>> forecasts = getForecasts(dayPart, longitude, latitude);
+
+            // Wait until we have the results from both calls
+            while (!places.isDone() && !forecasts.isDone()) {
+            }
+            dateOption.places = places.get();
+            dateOption.forecasts =  forecasts.get();
+        } catch (Exception e) {
             // TODO handle exception
         }
 
         return dateOption;
+    }
+
+    @Async
+    private Future<List<Forecast>> getForecasts(String dayPart, BigDecimal longitude, BigDecimal latitude) throws IOException {
+        return new AsyncResult<>(weatherService.retrieveWeather(latitude, longitude, dayPart));
+    }
+
+    @Async
+    private Future<List<Place>> getPlacesNearLocation(String type, BigDecimal radius, BigDecimal longitude, BigDecimal latitude) {
+        return new AsyncResult<>(placesService.getPlacesNearLocation(latitude, longitude, type, radius));
     }
 
 }
