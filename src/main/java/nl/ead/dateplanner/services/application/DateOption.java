d209b8d14a6ceb317da2c09030870f93ff81b7e0
Bobby Bouwmann <bobbybouwmann@gmail.com>
2015-10-08 10:14:16 +0200

    Refactor code

--- src/main/java/nl/ead/dateplanner/services/application/DateOption.java
+++ src/main/java/nl/ead/dateplanner/services/application/DateOption.java
@@ -3,19 +3,12 @@ package nl.ead.dateplanner.services.application;
 import nl.ead.dateplanner.services.application.google.Place;
 import nl.ead.dateplanner.services.application.openweather.Forecast;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class DateOption {
 
-<<<<<<< HEAD
-    public List<Forecast> forecasts = new ArrayList<>();
-
-    public List<Place> places = new ArrayList<>();
-=======
     public Place place;
 
     public List<Forecast> forecast;
->>>>>>> dev
 
 }
--- src/main/java/nl/ead/dateplanner/services/business/DateTaskFactory.java
+++ src/main/java/nl/ead/dateplanner/services/business/DateTaskFactory.java
@@ -6,6 +6,7 @@ public class DateTaskFactory {
 
     public IDateTaskService create() {
         DateFinderFactory factory = new DateFinderFactory();
+
         return new DateTaskService(factory.create());
     }
 
