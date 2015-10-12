9d9ecbff741b8ab502631deaee177324eb984f75
Glowbal <thomasskooi@live.nl>
2015-10-12 10:00:03 +0200

    Refactor and comment DateFinderService

--- src/main/java/nl/ead/dateplanner/services/application/DateFinderService.java
+++ src/main/java/nl/ead/dateplanner/services/application/DateFinderService.java
@@ -16,44 +16,50 @@ import java.util.concurrent.Future;
 
 public class DateFinderService implements IDateFinderService {
 
-    private IPlacesService placesService;
-
-    private IWeatherService weatherService;
-
-    private IGeoLocationService locationService;
-
+    /**
+     * Create a DateFinderService
+     * @param placesService Service to find places near coordinates
+     * @param weatherService Service to find the forecasts near coordinates
+     * @param locationService Service to convert name to coordinates
+     */
     public DateFinderService(IPlacesService placesService, IWeatherService weatherService, IGeoLocationService locationService) {
         this.placesService = placesService;
         this.weatherService = weatherService;
         this.locationService = locationService;
     }
 
+    /**
+     * Get date options with places and forecasts
+     * @param type The type of places
+     * @param location The location where we want to search for places
+     * @param dayPart What part of the day do we want to plan a date
+     * @param radius How far away from the location do we want to find dates
+     * @return Date options with places and forecasts. Empty list if nothing could be found
+     * @throws IOException
+     */
     public List<DateOption> getDateOptions(String type, String location, String dayPart, Double radius) throws IOException {
         // Find the coordinates for the location
         GeoLocation geoLocation = locationService.getLocation(location);
         Double longitude = geoLocation.longitude.doubleValue();
         Double latitude = geoLocation.latitude.doubleValue();
 
-        try {
-            List<Place> places = getPlacesNearLocation(latitude, longitude, type, radius);
+        // Get all places near the coordinates of the location
+        List<Place> places = getPlacesNearLocation(latitude, longitude, type, radius);
 
+        try {
+            // We want to get the actual forecast for each returned place
+            // We don't have to wait until a previous request for a forecast has arrived
+            // So we do everything async
             List<Future<List<Forecast>>> asyncForecasts = new ArrayList<>();
             for (Place place : places) {
                 Future<List<Forecast>> forecasts = getForecasts(place.latitude, place.longitude, dayPart);
                 asyncForecasts.add(forecasts);
             }
 
-            Boolean isWaiting = true;
-            while (isWaiting) {
-                isWaiting = false;
+            // Wait until we have received all forecasts
+            while (!hasReceivedAllForecasts(asyncForecasts)) {}
 
-                for (Future<List<Forecast>> a : asyncForecasts) {
-                    if (!a.isDone()) {
-                        isWaiting = true;
-                    }
-                }
-            }
-
+            // Combine the places and forecasts into dateOptions
             List<DateOption> dateOptions = new ArrayList<>();
             for (int i = 0; i < places.size(); i++) {
                 DateOption dateOption = new DateOption();
@@ -61,12 +67,10 @@ public class DateFinderService implements IDateFinderService {
                 dateOption.forecast = asyncForecasts.get(i).get();
                 dateOptions.add(dateOption);
             }
-
             return dateOptions;
         } catch (Exception e) {
             // TODO handle exception
         }
-
         return new ArrayList<>();
     }
 
@@ -78,5 +82,21 @@ public class DateFinderService implements IDateFinderService {
     private List<Place> getPlacesNearLocation(Double latitude, Double longitude, String type, Double radius) {
         return (placesService.getPlacesNearLocation(latitude, longitude, type, radius));
     }
+
+    private Boolean hasReceivedAllForecasts(List<Future<List<Forecast>>> asyncForecasts) {
+        for (Future<List<Forecast>> a : asyncForecasts) {
+            if (!a.isDone()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    private IPlacesService placesService;
+
+    private IWeatherService weatherService;
+
+    private IGeoLocationService locationService;
 
 }
